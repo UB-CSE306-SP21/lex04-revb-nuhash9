0d3d9c952c069e7adb627b2fbd92433f8ce5e272
Nuhash Chowdhury <nuhashchowdhury@outlook.com>
2021-02-13 06:41:12 -0500

    add comment at the end of egg.js, broccoli.py, and create celery.html

--- broccoli.py
+++ broccoli.py
@@ -1,1 +1,2 @@
 print("broccolis are green, doesn't mean it's money!")
+# if anyone asks, Nuhash said so.
--- egg.js
+++ egg.js
@@ -1,1 +1,2 @@
 console.log("eggs are good for you\n")
+// if someone asks, Nuhash said so.
